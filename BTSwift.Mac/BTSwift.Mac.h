15fbfcb7739696c6ec79bcffdfd6aa12772d2583
Eric Patey <eric@patey.com>
2015-12-29 08:32:04 -0500

    Update to iOS and Mac targets.

---
+++ BTSwift.Mac/BTSwift.Mac.h
@@ -0,0 +1,19 @@
+//
+//  BTSwift.Mac.h
+//  BTSwift.Mac
+//
+//  Created by Eric Patey on 12/29/15.
+//  Copyright © 2015 Eric Patey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BTSwift.Mac.
+FOUNDATION_EXPORT double BTSwift_MacVersionNumber;
+
+//! Project version string for BTSwift.Mac.
+FOUNDATION_EXPORT const unsigned char BTSwift_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTSwift_Mac/PublicHeader.h>
+
+
---
+++ BTSwift_iOS/BTSwift_iOS.h
@@ -0,0 +1,19 @@
+//
+//  BTSwift_iOS.h
+//  BTSwift_iOS
+//
+//  Created by Eric Patey on 12/29/15.
+//  Copyright © 2015 Eric Patey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BTSwift_iOS.
+FOUNDATION_EXPORT double BTSwift_iOSVersionNumber;
+
+//! Project version string for BTSwift_iOS.
+FOUNDATION_EXPORT const unsigned char BTSwift_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTSwift_iOS/PublicHeader.h>
+
+
